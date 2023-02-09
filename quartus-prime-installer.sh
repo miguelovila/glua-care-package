9d47eb01b9cba830d3071cfffebcf1e630111648
miguelovila <miguelovila@ua.pt>
2023-02-09 01:10:00 +0000

    WIP missing dependencies checks

--- quartus-prime-installer.sh
+++ quartus-prime-installer.sh
@@ -15,3 +15,15 @@ $DEBUG && { echo -e "\e[1;33m[ Debug ]\e[0m Checking where I'm running..."; };
 os_distro_name="$(lsb_release -is)"
 os_distro_release="$(lsb_release -sr)"
 $DEBUG && { echo -e "\e[1;33m[ Debug ]\e[0m I'm running in a $os_distro_name $os_distro_release machine!"; };
+
+has_distrobox=true
+has_curl=true
+has_podman=true
+has_docker=true
+
+$DEBUG && { echo -e "\e[1;33m[ Debug ]\e[0m Checking for missing dependencies..."; };
+command -v distrobox > /dev/null || { has_distrobox=false; }
+command -v curl > /dev/null || { has_curl=false; }
+command -v podman > /dev/null || { has_podman=false; }
+command -v docker > /dev/null || { has_docker=false; }
+$DEBUG && { echo -e "\e[1;33m[ Debug ]\e[0m I got this: has_distrobox=$has_distrobox, has_curl=$has_curl, has_podman=$has_podman, has_docker:$has_docker!"; };
